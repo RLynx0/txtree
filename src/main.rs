e9166cb7d3ccd1d00b797893ed374d0daa041c20
RLynx0 <luc.signin96@gmail.com>
2024-03-13 15:10:54 +0100

    stdin support

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,7 @@
-use std::process::exit;
+use std::{
+    io::{self, BufRead},
+    process::exit,
+};
 
 use clap::Parser;
 use txtree::{parser::parse_nodes, prelude::*};
@@ -27,27 +30,44 @@ fn main() {
             .build(),
     );
     if opt.debug {
-        eprintln!("{:#?}\n", render_mode);
+        eprintln!("{:#?}", render_mode);
         eprintln!("Demo Grid {{\n{}\n}}\n", render_mode.demo_grid());
     }
 
     let parse_mode = ParseMode::new(opt.delimiter, opt.brackets);
     if opt.debug {
-        eprintln!("{:#?}\n", parse_mode);
+        eprintln!("{:#?}", parse_mode);
     }
 
-    let parsed = match parse_nodes(&opt.input.join(" "), parse_mode) {
-        Ok(("", p)) => p,
-        Ok((s, _)) => {
-            eprintln!("Failed to parse input, on {:?}.", s);
-            exit(1);
+    if opt.input.is_empty() {
+        let stdin = io::stdin();
+        for line in stdin.lock().lines() {
+            let line = line.expect("Failed to read from stdin");
+            match parse_and_render(&line, parse_mode.clone(), opt.debug) {
+                Ok(s) => println!("{}", s),
+                Err(e) => eprintln!("{}", e),
+            };
         }
-        Err(e) => {
-            eprintln!("Failed to parse input. {:?}", e);
-            exit(1);
+    } else {
+        match parse_and_render(&opt.input.join(" "), parse_mode, opt.debug) {
+            Ok(s) => println!("{}", s),
+            Err(e) => {
+                eprintln!("{}", e);
+                exit(1)
+            }
         }
-    };
-    if opt.debug {
-        eprintln!("Parsed Nodes {:#?}\n", parsed);
     }
+}
+
+fn parse_and_render(input: &str, parse_mode: ParseMode, debug: bool) -> Result<String, String> {
+    let parsed = match parse_nodes(input, parse_mode) {
+        Ok(("", p)) => p,
+        Ok((s, _)) => return Err(format!("Failed to parse input, on {:?}.", s)),
+        Err(e) => return Err(format!("Failed to parse input. {:?}", e)),
+    };
+    if debug {
+        eprintln!("Parsed Nodes {:#?}", parsed);
+    }
+
+    Ok(String::new())
 }
